1fc44a8401d185fba3965fffdd6509fcd7798aab
Halasipeter <deprogrammed993@gmail.com>
2023-07-08 14:28:39 +0200

    Update docker.sh

--- docker.sh
+++ docker.sh
@@ -13,31 +13,30 @@ echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docke
 
 sudo apt update
 
+#auto-run docker on boot
 apt-cache policy docker-ce
 
 # Check if Docker is already installed
-if ! command -v docker &> /dev/null; then
     echo "Installing Docker..."
-    sudo apt update
-    if ! sudo apt install -y docker.io; then
-        echo "Failed to install Docker."
-        exit 1
-    fi
-    echo "Docker installation completed."
-else
-    echo "Docker is already installed."
-fi
+    if ! sudo apt install -y docker.io; 
+        then
+            echo "Failed to install Docker!"
+            exit 1
+        fi
+            echo "Docker installation completed"
+
 
 sudo systemctl status docker
 
 # Check if Docker Compose is already installed
-if ! command -v docker-compose &> /dev/null; then
     echo "Installing Docker Compose..."
-    if ! sudo apt install -y docker-compose; then
-        echo "Failed to install Docker Compose."
-        exit 1
-    fi
-    echo "Docker Compose installation completed."
-else
-    echo "Docker Compose is already installed."
-fi
+    sudo apt update
+    if ! sudo apt install -y docker-compose; 
+        then
+            echo "Failed to install Docker Compose!"
+            exit 1
+        fi
+            echo "Docker Compose installation completed"
+
+sudo apt update
+echo "All installation have been completted"
