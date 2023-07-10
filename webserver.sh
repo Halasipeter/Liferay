0aa9986bd79323db1666378bc21011c4ffe3c4aa
Halasipeter <deprogrammed993@gmail.com>
2023-07-10 12:29:52 +0200

    Update webserver.sh

--- webserver.sh
+++ webserver.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
-docker build -t web-server .
-docker run -d -p 80:80 --restart=always --name web-server web-server
-docker cp image.jpg web-server:/usr/local/apache2/htdocs/Home/Desktop/liferay/image.jpg
+sudo docker build -t web-server .
+sudo docker run -d -p 80:80 --restart=always --name web-server web-server
+sudo docker cp ~/Desktop/liferay/index.html web-server:/usr/local/apache2/htdocs/index.html
+sudo docker cp ~/Desktop/liferay/image.jpg web-server:/usr/local/apache2/htdocs/image.jpg
