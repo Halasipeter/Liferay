7b735e7427758c09c0015e70e2f35a0545ae0778
Halasipeter <deprogrammed993@gmail.com>
2023-07-10 12:11:02 +0200

    Update webserver.sh

--- webserver.sh
+++ webserver.sh
@@ -2,3 +2,4 @@
 
 docker build -t web-server .
 docker run -d -p 80:80 --restart=always --name web-server web-server
+docker cp image.jpg web-server:/usr/local/apache2/htdocs/Home/Desktop/liferay/image.jpg
